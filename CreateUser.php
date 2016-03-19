aaa64f55472295de5496b6ad664bb0cf770f1b6e
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-19 15:46:33 -0500

    Add CreateUser.php validation

--- CreateUser.php
+++ CreateUser.php
@@ -1,10 +1,8 @@
-<?
+<?php
 
 //connect to the database
 
 $mysqli = new mysqli("https://mysql.eecs.ku.edu",djoseph,f2TUteC4dQRqL7jR,djoseph);
-$username = $_POST["name"];*/
-echo $username;	
 
 //Check connection
 if($mysqli->connect_errno){
@@ -13,16 +11,19 @@ if($mysqli->connect_errno){
 	exit();
 }
 
-$insert = "INSERT INTO Users (user_id) VALUES $username";
+//Since connection has been established, take username
+$username = $_POST[user];
+
+$insert = "INSERT INTO Users (user) VALUES ('$username');";
+
+if($result = $mysqli->query($insert)){
+	echo "$username was succesfully added.\n";
+}
+else{
+	echo "ERROR: $username does not fill preconditions.\n";
+}
 
 
 $mysqli->close();
-	
-	
-	
-	
-	
-	
-	
 	
 ?>
