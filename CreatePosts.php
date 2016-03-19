786154e88bcfb6896ca7300f652e383d80bdf842
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-19 16:59:41 -0500

    Check uniqueness

--- CreatePosts.php
+++ CreatePosts.php
@@ -26,14 +26,16 @@ echo "We're working!";
 $exist = $mysqli->query("SELECT user_id FROM Users WHERE user_id = '$username'");
 
 echo "We're still working!";
-/**
+echo $exist->num_rows;
+
 if($exist->num_rows == 0){
 	echo "Only existing users may post!";
 	mysqli->close();
 	exit();
 }
-echo "Still here!";
 
+echo "Still here!";
+/*
 //user exists, so add posts to database
 
 $postContent = "INSERT INTO Posts (author_id,content) VALUES ('$username','$content')";
