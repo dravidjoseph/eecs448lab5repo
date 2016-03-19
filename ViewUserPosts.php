db821e76dadd6bd6bbf360b96418ce6c62933b2e
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-19 17:51:51 -0500

    Add ViewUserPosts

---
+++ ViewUserPosts.php
@@ -0,0 +1,23 @@
+<?php
+	
+$username = $_POST["user"];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu","djoseph","f2TUteC4dQRqL7jR","djoseph");
+
+if($mysqli->connect_errno){
+	printf("Connection Failed: %s\n",$mysqli->connect_error);
+	exit();
+}
+
+$userPosts = $mysqli->query("SELECT content FROM Posts WHERE author_id = '$username'");
+
+echo '<table border = "1" style = "width = 100%">';
+echo '<th>Post From '.$username'</th>';
+
+while($row = mysqli_fetch_array($userPosts)){
+	$post = $row['content'];
+	echo '<tr><td>'.$post'</td></tr>'
+}
+
+echo '</table>'		
+?>
