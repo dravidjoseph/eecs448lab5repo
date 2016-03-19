4637096b7848539f91ada51c4ca0f2a21565415e
Dravid Joseph <dravidjoseph@gmail.com>
2016-03-19 16:21:26 -0500

    Validate user creation

--- CreateUser.php
+++ CreateUser.php
@@ -14,17 +14,30 @@ if($mysqli->connect_errno){
 //Since connection has been established, take username
 $username = $_POST[user];
 
-//SQL query
-$insert = "INSERT INTO Users (user_id) VALUES ('$username')";
-
-if($result = $mysqli->query($insert)){
-	echo "$username was succesfully added.\n";
+if($username == ""){
+	echo "Username cannot be left blank!";
+	$mysqli->close();
+	exit();
 }
+
+//Query to check for duplicates
+$dupCheck = $mysqli->query("SELECT user_id FROM Users WHERE user_id = '$username'");
+if($dupCheck->num_rows != 0){
+	echo "ERROR: Duplicate username.\n";
+}
+
 else{
-	echo "ERROR: $username does not fill preconditions.\n";
+//SQL query
+	$insert = "INSERT INTO Users (user_id) VALUES ('$username')";
+
+	if($result = $mysqli->query($insert)){
+		echo "$username was succesfully added.\n";
+	}
+	else{
+		echo "ERROR: $username does not fill preconditions.\n";
+	}
+
 }
-
-
 $mysqli->close();
 	
 ?>
